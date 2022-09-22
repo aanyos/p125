13454e67f3c22556ce392c59173d5589188ed6aa
aanyos <85759900+aanyos@users.noreply.github.com>
2022-09-21 20:52:56 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 noseX = 0;
 noseY = 0;
 
+mouthX = 0;
+mouthY = 0;
+
 function preload() {
     mustache = loadImage("https://i.postimg.cc/3JsbGz43/mustache.png")
     lipstick = loadImage("https://i.postimg.cc/dtbGnYjS/lipstick.png")
@@ -24,7 +27,7 @@ function modelLoaded () {
 function draw() {
     image(video, 0, 0, 300, 300);
     image(mustache, noseX, noseY, 70, 50);
-    image(lipstick, noseX, noseY, 70, 50);
+    image(lipstick, mouthX, mouthY, 70, 50);
 }
 
 function take_snapshot() {
@@ -36,14 +39,14 @@ function gotPoses(results)
     if(results.length > 0)
     {
         console.log(results);
-        noseX = results[0].pose.nose.x+35;
-        noseY = results[0].pose.nose.y-10;
+        
+        noseX = results[0].pose.nose.x-35;
+        noseY = results[0].pose.nose.y;
+        
+        mouthX = results[0].pose.nose.x-35;
+        mouthY = results[0].pose.nose.y+25;
+
         console.log("mustache x = " + results[0].pose.nose.x);
         console.log("mustache y = " + results[0].pose.nose.y);
-        console.log(results);
-        noseX = results[0].pose.nose.x-20;
-        noseY = results[0].pose.nose.y+50;
-        console.log("lipstick x = " + results[0].pose.nose.x);
-        console.log("lipstick y = " + results[0].pose.nose.y);
     }
 }
